b50d07fd08ab82d74a20a605046d0b2da55755d8
202116302 <wls5258@naver.com>
2026-01-24 10:50:26 +0900

    도커 최적화

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    minify: 'esbuild',
+    target: 'es2022',
+  },
 })
